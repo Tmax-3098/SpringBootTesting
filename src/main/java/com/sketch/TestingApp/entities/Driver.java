42a844d85c842dd700a42e8693a3ddc6decacb47
Tmax-3098 <ishaan.3098@gmail.com>
2026-02-24 19:43:48 +0530

    testcontainers and testing persistance layer

--- src/main/java/com/sketch/TestingApp/entities/Driver.java
+++ src/main/java/com/sketch/TestingApp/entities/Driver.java
@@ -1,23 +1,18 @@
 package com.sketch.TestingApp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Driver {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
---
+++ src/test/java/com/sketch/TestingApp/repositories/DriverRepoTest.java
@@ -0,0 +1,59 @@
+package com.sketch.TestingApp.repositories;
+
+import com.sketch.TestingApp.TestContainerConfiguration;
+import com.sketch.TestingApp.entities.Driver;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@Import(TestContainerConfiguration.class)
+@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+class DriverRepoTest {
+
+    @Autowired
+    private DriverRepo repo;
+
+    private Driver driver;
+
+    @BeforeEach
+    void setUp(){
+        driver = Driver.builder()
+                .name("Carlos")
+                .team("williams")
+                .exp(10)
+                .number(45)
+                .build();
+    }
+
+    @Test
+    void testFindByTeam_whenTeamIsValid_thenReturnDrivers() {
+        //Arrange
+        repo.save(driver);
+        //Act
+        List<Driver> driverList = repo.findByTeam("williams");
+        //Assert
+        assertThat(driverList).isNotEmpty();
+        assertThat(driverList).isNotNull();
+        assertThat(driverList.get(0).getTeam()).isEqualTo("williams");
+    }
+
+    @Test
+    void testFindByTeam_whenTeamIsNotValid_thenReturnEmptyList(){
+
+        repo.save(driver);
+        List<Driver> driverList = repo.findByTeam("redbull");
+        assertThat(driverList).isNotNull();
+        assertThat(driverList).isEmpty();
+
+    }
+}
